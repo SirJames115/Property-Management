b074941774a2058aeb1fdc6b288dc6c8a4195f49
James Othniel <jamesothniel7@gmail.com>
2023-12-29 07:40:35 -0800

    Pages added and routes configured for each page`

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,25 @@
-export default function App() {
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+
+function App() {
   return (
-    <h1 className="text-3xl font-bold underline ">
-      Hello world!
-    </h1>
-  )
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" index element={<Home />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
+
+export default App;
