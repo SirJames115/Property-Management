3bb33d58fcf597a37d2b8ec2180c88cef72b9b6e
James Othniel <jamesothniel7@gmail.com>
2024-01-01 14:45:44 -0800

    Create a middleware and a function to handdle error

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,40 +1,36 @@
 import asyncHandler from "express-async-handler";
 import User from "../models/userModel.js";
-import bcryptjs from 'bcryptjs'
+import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
 
-export const authController = asyncHandler(async (req, res) => {
+export const authController = asyncHandler(async (req, res, next) => {
   const { username, email, password } = req.body;
 
-//   
-const emailExists = await User.findOne({email})
-const usernameExixts = await User.findOne({username})
+  //
+  // const emailExists = await User.findOne({email})
+  // const usernameExixts = await User.findOne({username})
 
-if(emailExists){
-    res.send({
-        Message: `The user with the email ${email} already exists`
-    })
-    console.log(`The user with the email ${email} already exists`);
-}
-if(usernameExixts){
-    res.send({
-        Message: `The user with the username ${username} already exists`
-    })
-    console.log(`The user with the username ${username} already exists`);
-}
-// 
+  // if(emailExists){
+  //     res.send({
+  //         Message: `The user with the email ${email} already exists`
+  //     })
+  //     console.log(`The user with the email ${email} already exists`);
+  // }
+  // if(usernameExixts){
+  //     res.send({
+  //         Message: `The user with the username ${username} already exists`
+  //     })
+  //     console.log(`The user with the username ${username} already exists`);
+  // }
+  //
 
-const hashedPassword = await bcryptjs.hash(password, 10)
-
+  const hashedPassword = await bcryptjs.hash(password, 10);
 
   const newUser = await new User({ username, email, password: hashedPassword });
   try {
-  await newUser.save();
-     res.status(201).json({ message: "User created successfully" });
+    await newUser.save();
+    res.status(201).json({ message: "User created successfully" });
   } catch (error) {
-    res.status(500).json({
-        Error: error
-    })
+    next(error);
   }
-
- 
 });
--- backend/server.js
+++ backend/server.js
@@ -27,3 +27,14 @@ app.listen(port, () => {
 
 app.use('/api/user', userRouter)
 app.use('/api/auth', authRouth)
+
+// Error handler
+app.use((err, req, res, next) =>{
+  const statusCode = err.statusCode || 500
+  const message = err.message || 'Internal server error'
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message
+  })
+})
