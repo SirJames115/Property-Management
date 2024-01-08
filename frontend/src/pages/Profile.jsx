d2f083591f0740b39bf5245883a78c989a5b3140
James Othniel <jamesothniel7@gmail.com>
2024-01-07 22:39:43 -0800

    complete update user functionality

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -8,15 +8,26 @@ import {
 } from "firebase/storage";
 import { toast } from "react-toastify";
 import { app } from "../firebase";
+import { useDispatch } from "react-redux";
+import {
+  updateUserStart,
+  updateUserSuccess,
+  updateUserFailure,
+} from "../redux/user/userSlice";
+// import { Toast } from "react-toastify/dist/components";
 
 function Profile() {
   const fileRef = useRef(null);
-  const { currentUser } = useSelector((state) => state.user);
+
+  const dispatch = useDispatch();
+
+  const { currentUser, loading, error } = useSelector((state) => state.user);
 
   const [file, setFile] = useState(undefined);
   const [filePerc, setFilePerc] = useState(0);
   const [fileUploadError, setFileUploadError] = useState(false);
   const [formData, setFormData] = useState({});
+  const [updateSuccess, setUpdateSuccess] = useState(false);
 
   // Console.logs
   // console.log(file);
@@ -54,12 +65,42 @@ function Profile() {
     );
   };
 
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      dispatch(updateUserStart());
+      const res = await fetch(`/api/user/update/${currentUser._id}`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(formData),
+      });
+
+      const data = await res.json();
+
+      if (data.success === false) {
+        dispatch(updateUserFailure(data.message));
+        return;
+      }
+
+      dispatch(updateUserSuccess(data));
+      setUpdateSuccess(true);
+      //To be removed at the end of the day
+      console.log(loading);
+    } catch (error) {
+      dispatch(updateUserFailure(error.message));
+    }
+  };
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7 uppercase">
         Profile
       </h1>
-      <form className="flex flex-col gap-4">
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
         <input
           onChange={(e) => setFile(e.target.files[0])}
           type="file"
@@ -89,30 +130,39 @@ function Profile() {
         </p>
         <input
           type="text"
+          defaultValue={currentUser.username}
+          onChange={handleChange}
           className="border p-3 rounded-lg"
           placeholder="Username"
           id="username"
         />
         <input
           type="email"
+          defaultValue={currentUser.email}
+          onChange={handleChange}
           className="border p-3 rounded-lg"
           placeholder="Email"
           id="email"
         />
         <input
           type="password"
+          onChange={handleChange}
           className="border p-3 rounded-lg"
           placeholder="Password"
           id="password"
         />
-        <button className="bg-slate-700 rounded-lg hover:opacity-95  disabled:opacity-80 uppercase text-white p-3">
-          Update
+        <button
+          disabled={loading}
+          className="bg-slate-700 rounded-lg hover:opacity-95  disabled:opacity-80 uppercase text-white p-3">
+          {loading ? "Loading..." : "Update"}
         </button>
       </form>
       <div className="flex justify-between mt-5">
         <span className="text-red-700 cursor-pointer">Delete Account</span>
         <span className="text-red-700 cursor-pointer">Sign out</span>
       </div>
+      <p hidden>{error ? toast.error(error) : ""}</p>
+      <p hidden>{updateSuccess ? toast.success("Updated successfully") : ""}</p>
     </div>
   );
 }
