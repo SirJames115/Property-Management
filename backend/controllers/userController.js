9092528bcbb3747ca2981179dd2776f79c47cbf9
James Othniel <jamesothniel7@gmail.com>
2024-01-09 22:40:45 -0800

    updated the clear cookie for delete user controller funcionality

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -40,7 +40,7 @@ export const deleteUser = AsyncHandler(async (req, res, next) => {
 
   try {
     await User.findByIdAndDelete(req.params.id);
-    res.cookie("access_token");
+    res.clearCookie("access_token");
     res.status(200).json("User has been deleted");
   } catch (error) {
     next(error);
