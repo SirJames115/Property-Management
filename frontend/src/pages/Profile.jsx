9e14d787e27aa6fc614ed4e7ad51a26690f626b9
James Othniel <jamesothniel7@gmail.com>
2024-01-21 20:09:42 -0800

    complete show listing functionality

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -39,6 +39,8 @@ function Profile() {
   const [fileUploadError, setFileUploadError] = useState(false);
   const [formData, setFormData] = useState({});
   const [updateSuccess, setUpdateSuccess] = useState(false);
+  const [showListingsError, setShowListingsError] = useState(false);
+  const [userListings, setUserListings] = useState([]);
 
   // Console.logs
   // console.log(file);
@@ -143,6 +145,22 @@ function Profile() {
     }
   };
 
+  const handleShowListings = async () => {
+    try {
+      setShowListingsError(false);
+      const res = await fetch(`/api/user/listings/${currentUser._id}`);
+      const data = await res.json();
+      if (data.success === false) {
+        setShowListingsError(true);
+        toast.error(data.message);
+      }
+      setUserListings(data);
+    } catch (error) {
+      setShowListingsError(true);
+      toast.error("Error showing listings!");
+    }
+  };
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7 uppercase">
@@ -221,8 +239,45 @@ function Profile() {
           Sign out
         </span>
       </div>
-      {/* <p hidden>{error ?  : ""}</p> */}
-      {/* <p hidden>{updateSuccess ?  : ""}</p> */}
+      <button
+        onClick={handleShowListings}
+        className="text-green-700 w-full hover:bg-green-700 hover:text-white transition-all delay-300 rounded-lg p-3">
+        Show listings
+      </button>
+      {userListings && userListings.length > 0 && (
+        <div className="flex flex-col">
+          <h1 className="text-center text-2xl font-semibold mt-7">
+            Your Listings
+          </h1>
+          {userListings.map((listing) => (
+            <div
+              key={listing._id}
+              className="border rounded-lg p-3 flex justify-between items-center gap-4 my-1 hover:bg-sky-100 delay-75">
+              <Link to={`/listing/${listing._id}`}>
+                <img
+                  src={listing.imageUrls[0]}
+                  alt="listing cover"
+                  className="h-16 w-16 object-contain"
+                />
+              </Link>
+              <Link
+                className="text-slate-700 font-semibold flex-1 hover:underline truncate"
+                to={`/listing/${listing._id}`}>
+                <p>{listing.name}</p>
+              </Link>
+
+              <div className="flex flex-col">
+                <button className="text-red-700 uppercase border p-1 rounded hover:bg-slate-200 hover:shadow-md delay-100">
+                  Delete
+                </button>
+                <button className="text-green-700 uppercase border p-1 rounded hover:bg-slate-200 hover:shadow-md delay-100">
+                  Edit
+                </button>
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
