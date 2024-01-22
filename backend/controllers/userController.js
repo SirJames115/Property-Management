1668e2ee543876198ba6da004129f016d059d909
James Othniel <jamesothniel7@gmail.com>
2024-01-21 18:40:36 -0800

    create get user listings api route

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,6 +2,7 @@ import AsyncHandler from "express-async-handler";
 import { errorHandler } from "../utils/error.js";
 import bcryptjs from "bcryptjs";
 import User from "../models/userModel.js";
+import Listing from "../models/listingModel.js";
 
 export const test = AsyncHandler(async (req, res) => {
   res.status(200).json({ msg: "Hello test!!" });
@@ -44,5 +45,18 @@ export const deleteUser = AsyncHandler(async (req, res, next) => {
     res.status(200).json("User has been deleted");
   } catch (error) {
     next(error);
+  }
+});
+
+export const getUserListings = AsyncHandler(async (req, res, next) => {
+  if (req.user.id === req.params.id) {
+    try {
+      const listings = await Listing.find({ userRef: req.params.id });
+      res.status(200).json(listings);
+    } catch (error) {
+      next(error);
+    }
+  } else {
+    return next(errorHandler(401, "You can only view your own listing!"));
   }
 });
