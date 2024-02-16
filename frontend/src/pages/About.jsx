ba3523a14b210cfdf823f511bd11470be4160355
James Othniel <jamesothniel7@gmail.com>
2024-02-16 15:04:15 +0100

    Complete homepage

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from "react";
 
 function About() {
   return (
-    <div>About</div>
-  )
+    <div className="px-4 py-20 gap-4">
+      <h1 className="font-bold text-3xl text-slate-700 mb-4">
+        About CodeIcon Estate
+      </h1>
+      <p className="text-slate-600">
+        At CodeIcon Estate, we strive to provide exceptional real estate
+        services tailored to meet the unique needs of our clients. Whether
+        you're buying, selling, or renting, we're here to guide you through
+        every step of the process with expertise and dedication.
+      </p>
+      <p className="text-slate-600">
+        Our platform was built with care and precision by CodeIcon Technologies,
+        a leading tech company specializing in innovative software solutions.
+        Led by the talented software engineer James Othniel, our platform
+        combines cutting-edge technology with user-friendly design to deliver a
+        seamless experience for our customers.
+      </p>
+      <p className="text-slate-600">
+        With a passion for excellence and a commitment to customer satisfaction,
+        we are dedicated to helping you achieve your real estate goals. Thank
+        you for choosing{" "}
+        <span className="font-semibold"> CodeIcon Estate –</span> where your
+        dreams become reality.
+      </p>
+    </div>
+  );
 }
 
-export default About
+export default About;
