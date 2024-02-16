1ff85d853d0e5db92530bb56d0e0ab3ce4788036
James Othniel <jamesothniel7@gmail.com>
2024-02-16 14:38:56 +0100

    complete home page

--- frontend/src/pages/CreateListing.jsx
+++ frontend/src/pages/CreateListing.jsx
@@ -154,6 +154,7 @@ export const CreateListing = () => {
         },
         body: JSON.stringify({ ...formData, userRef: currentUser._id }),
       });
+
       const data = await res.json();
       setLoading(false);
       if (data.success === false) {
